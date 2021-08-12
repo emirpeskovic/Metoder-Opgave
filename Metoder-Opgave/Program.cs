85367d9f5af0440564f7c60ee0b7a6be61cb031c
Emir <me@emir.dk>
2021-08-12 10:44:52 +0200

    Opgave 3 + some changes
    Added Continue() function to be called after every method
    Usage of new message option in GetInput<T>

--- Metoder-Opgave/Program.cs
+++ Metoder-Opgave/Program.cs
@@ -7,11 +7,16 @@ namespace Metoder_Opgave
     {
         static void Main(string[] args)
         {
-            int tal1 = ConsoleEx.GetInput<int>().AndClear();
-            int tal2 = ConsoleEx.GetInput<int>().AndClear();
+            Opgave1(ConsoleEx.GetInput<int>("Indtast tal 1: ").AndClear(), ConsoleEx.GetInput<int>("Indtast tal 2: ").AndClear());
+            Opgave2(ConsoleEx.GetInput<int>("Indtast tal 1: ").AndClear(), ConsoleEx.GetInput<int>("Indtast tal 2: ").AndClear());
+            Opgave3(ConsoleEx.GetInput<string>("Indtast dit navn: ").AndClear(), ConsoleEx.GetInput<int>("Indtast din alder: ").AndClear());
+        }
 
-            Opgave1(tal1, tal2);
-            Opgave2(tal1, tal2);
+        static void Continue()
+        {
+            Console.WriteLine("\r\nPress any key to continue.");
+            Console.ReadKey();
+            Console.Clear();
         }
 
         static void Opgave1(int tal1, int tal2)
@@ -28,7 +33,9 @@ namespace Metoder_Opgave
                 $"Division (Int): {div1}\r\n" + // 1.2
                 $"Division (Float): {div2}\r\n" + // 1.2
                 $"Rem: {rem}\r\n" + // 1.3
-                $"Pow: {pow}\r\n"); // 1.4
+                $"Pow: {pow}"); // 1.4
+
+            Continue();
         }
 
         static void Opgave2(int tal1, int tal2)
@@ -38,7 +45,23 @@ namespace Metoder_Opgave
             double tal3 = Math.Sqrt(Math.Pow(tal1, 2) + Math.Pow(tal2, 2));
 
             Console.WriteLine($"c = {tal3}\r\n" + // 2.1
-                $"Bigger Number: {(tal1 != tal2 ? tal1 > tal2 ? "First" : "Second" : "Neither")}\r\n"); // 2.2
+                $"Bigger Number: {(tal1 != tal2 ? tal1 > tal2 ? "First" : "Second" : "Neither")}"); // 2.2
+
+            Continue();
+        }
+
+        static void Opgave3(string name, int age)
+        {
+            if (age < 3)
+                Console.WriteLine($"{name}, du må gå med ble"); // 3.1
+            else if (age >= 3 && age <= 15)
+                Console.WriteLine($"{name}, du må ingenting"); // 3.2
+            else if (age > 15 && age < 18)
+                Console.WriteLine($"{name}, du må drikke"); // 3.3
+            else if (age > 18)
+                Console.WriteLine($"{name}, du må stemme og køre bil"); // 3.4
+
+            Continue();
         }
     }
 }
