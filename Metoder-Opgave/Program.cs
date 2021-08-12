92ecc1417545b5c25c5d2b27bbd415b25b867afa
Emir <me@emir.dk>
2021-08-12 10:16:29 +0200

    Opgave 2

--- Metoder-Opgave/Program.cs
+++ Metoder-Opgave/Program.cs
@@ -11,6 +11,7 @@ namespace Metoder_Opgave
             int tal2 = ConsoleEx.GetInput<int>().AndClear();
 
             Opgave1(tal1, tal2);
+            Opgave2(tal1, tal2);
         }
 
         static void Opgave1(int tal1, int tal2)
@@ -27,7 +28,17 @@ namespace Metoder_Opgave
                 $"Division (Int): {div1}\r\n" + // 1.2
                 $"Division (Float): {div2}\r\n" + // 1.2
                 $"Rem: {rem}\r\n" + // 1.3
-                $"Pow: {pow}"); // 1.4
+                $"Pow: {pow}\r\n"); // 1.4
+        }
+
+        static void Opgave2(int tal1, int tal2)
+        {
+            "Opgave 2: ".WriteLine();
+
+            double tal3 = Math.Sqrt(Math.Pow(tal1, 2) + Math.Pow(tal2, 2));
+
+            Console.WriteLine($"c = {tal3}\r\n" + // 2.1
+                $"Bigger Number: {(tal1 != tal2 ? tal1 > tal2 ? "First" : "Second" : "Neither")}\r\n"); // 2.2
         }
     }
 }
